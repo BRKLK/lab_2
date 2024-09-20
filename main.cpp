b9f290f1ca2e6994d17ef0eb45ee3bc52ff9897d
BRKLK <burkalyek@gmail.com>
2024-09-20 22:06:06 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    short a;
+    cout << "Input a value: "; cin >> a;
+    cout << "The cude volume is " << pow(a, 3);
+
+
+    return 1;
+}
